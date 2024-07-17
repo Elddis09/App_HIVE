c36ff8a9e43d6a643078278f6d1b3173964008ed
Nurhasnh <114034337+Nurhasnh@users.noreply.github.com>
2024-07-18 03:15:17 +0700

    Update beranda_mading.dart

--- lib/pages/beranda_mading.dart
+++ lib/pages/beranda_mading.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 // import 'package:flutter_svg/flutter_svg.dart';
-import 'package:google_fonts/google_fonts.dart';
+//import 'package:google_fonts/google_fonts.dart';
 
 class BerandaMading extends StatelessWidget {
   final List<Article> articles = [
